e07efc775f31f8abc205b8038d7e4f67491e939c
DRAP-3312 <davidake117@gmail.com>
2025-05-18 13:51:04 -0500

    config bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { DynamicModule } from './dynamic/dynamic.module';
 
 @Module({
-  imports: [DynamicModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        uri: `mongodb://${configService.get('MONGO_ROOT_USERNAME')}:${configService.get('MONGO_ROOT_PASSWORD')}@localhost:${configService.get('MONGO_PORT')}`,
+        dbName: configService.get('MONGO_DATABASE'),
+      }),
+      inject: [ConfigService],
+    }),
+    DynamicModule,
+  ],
 })
 export class AppModule {}
